6e147d67053543859084a688e2ba0c2bec2f5348
Chenghao Li <lich2000117@gmail.com>
2020-08-17 18:37:52 +0800

    try this player controller model

---
+++ Assets/Scripts/PlayerCamManager/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//get the text moving
+public class Move : MonoBehaviour
+{
+    public Vector3 StartPos = new Vector3(-3.48f,-2.36f,-5.4f);
+    public Vector3 EndPos = new Vector3(-3.48f,6.88f,-5.4f);
+    public double Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = StartPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(transform.position.y >= EndPos.y)){
+            transform.position += new Vector3(0, (float)Speed*Time.deltaTime,0);
+        }
+    }
+}
