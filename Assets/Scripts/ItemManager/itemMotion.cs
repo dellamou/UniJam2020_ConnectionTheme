3405c1ac076c711c25f4236db3556140b456d58d
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-08-19 22:55:57 +0800

    try try

---
+++ Assets/Scripts/ItemManager/itemMotion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Rotate items when it's on the ground, jump when the player is near enough
+public class itemMotion : MonoBehaviour
+{
+    public float spinSpeed=5;
+    public GameObject player;
+    public float jumpRadius = 6;
+
+    public float yLimit = 0.3f;
+    public float moveSpeed = 0.2f;
+
+    private bool back =false;
+    private bool startJump = false;
+    private float distance;
+    private Vector3 originPos;
+    void Start()
+    {
+        originPos = this.transform.position;
+    }
+    void Update()
+    {
+        //rotate
+        this.transform.localRotation *= Quaternion.AngleAxis(Time.deltaTime * spinSpeed, new Vector3(0.0f, 1.0f, 0.0f));
+    
+        //jump when distance is close enough
+        distance = Vector3.Distance(this.transform.position, player.transform.position);
+            if (distance <= jumpRadius){
+                startJump=true;
+            } 
+
+            if (startJump){
+                if (this.transform.position.y > originPos.y + yLimit){
+                    back = true;
+                }
+                else if (this.transform.position.y < originPos.y){
+                    back = false;
+                } 
+                if (back){
+                    this.transform.localPosition += new Vector3 (0.0f,-moveSpeed*Time.deltaTime,0.0f);
+                }
+                else{
+                    this.transform.localPosition += new Vector3 (0.0f,moveSpeed*Time.deltaTime,0.0f);
+                }
+            }
+            if (distance > jumpRadius){
+                startJump=false;
+            } 
+    }
+}
