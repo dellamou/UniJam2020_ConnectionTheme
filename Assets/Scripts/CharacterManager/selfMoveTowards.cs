81b7f28e67fe1a00575b53196562b09ed1ba4aa9
Danlei Mou <dmou@student.unimelb.edu.au>
2020-08-22 07:59:10 +0800

    added endCG and scene switching

---
+++ Assets/Scripts/CharacterManager/selfMoveTowards.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class selfMoveTowards : MonoBehaviour
+{
+    public float walkingSpeed = 1.5f;
+    private Vector3 target;
+
+    private IEnumerator coroutine;
+
+    public void moveTowards(Vector3 target)
+    {
+        this.target = target;
+        coroutine = WaitSwitchScene(1.5f);
+        StartCoroutine(coroutine);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, walkingSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator WaitSwitchScene(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("GoodEnd");
+    }
+}
---
+++ Assets/Scripts/PlayerCamManager/ChangeCameraWhenEnterTheRoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraWhenEnterTheRoom : MonoBehaviour
+{
+
+    public GameObject endCamera;
+    public GameObject model;
+    public GameObject friend;
+
+    public GameObject connection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").SetActive(true);
+        //GameObject.FindGameObjectWithTag("EndCamera").SetActive(false);
+        //GameObject.FindGameObjectWithTag("EndPlayer").SetActive(false);
+        endCamera.SetActive(false);
+        model.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "EndTrigger")
+        {
+            Destroy(collision.gameObject);
+            this.GetComponent<MoveCam>().enabled = false;
+            connection.SetActive(false);
+            GameObject.FindGameObjectWithTag("MainCamera").SetActive(false);
+            //GameObject.FindGameObjectWithTag("EndCamera").SetActive(true);
+            //GameObject.FindGameObjectWithTag("EndPlayer").SetActive(true);
+            endCamera.SetActive(true);
+            model.SetActive(true);
+            model.GetComponent<selfMoveTowards>().moveTowards(friend.transform.position);
+        }
+    }
+}
