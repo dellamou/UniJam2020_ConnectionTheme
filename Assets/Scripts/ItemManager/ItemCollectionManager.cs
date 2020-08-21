303266d8d15174d05aff1899ababe5c71e1342c2
Danlei Mou <dmou@student.unimelb.edu.au>
2020-08-22 01:42:18 +0800

    adjust torch icon with q, add press q text display

--- Assets/Scripts/ItemManager/ItemCollectionManager.cs
+++ Assets/Scripts/ItemManager/ItemCollectionManager.cs
@@ -47,7 +47,7 @@ public class ItemCollectionManager : MonoBehaviour
             case "Torch":
                 PhysicalTorch.SetActive(true);
                 this.torch = true;
-                TopScreenNotice.text = "You picked up Torch";
+                TopScreenNotice.text = "You picked up Torch, now you could press Q to use it";
                 break;
             case "Photo":
                 this.photo = true;
