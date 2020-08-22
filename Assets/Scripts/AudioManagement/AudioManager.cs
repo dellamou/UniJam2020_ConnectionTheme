bdfd019c8767bdc8b52366b3465259e4f97436d9
Chenghao Li <lich2000117@gmail.com>
2020-08-22 20:42:06 +0800

    added torch sound FX

--- Assets/Scripts/AudioManagement/AudioManager.cs
+++ Assets/Scripts/AudioManagement/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public AudioClip[] audioClips;
-
+    private bool mapOpened = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +18,19 @@ public class AudioManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            this.GetComponent<AudioSource>().clip = audioClips[0];
-            this.GetComponent<AudioSource>().Play();
+            if (mapOpened){
+                this.mapOpened = !this.mapOpened;
+                this.GetComponent<AudioSource>().clip = audioClips[1];
+                this.GetComponent<AudioSource>().Play();
+            }
+            else {
+                this.mapOpened = !this.mapOpened;
+                this.GetComponent<AudioSource>().clip = audioClips[2];
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+        else {
+
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
