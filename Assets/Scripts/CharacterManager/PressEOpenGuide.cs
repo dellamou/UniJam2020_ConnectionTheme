5671dc1bacb1b807526d45f98b4bad822318c829
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-08-22 21:14:21 +0800

    added good end ending scene

---
+++ Assets/Scripts/CharacterManager/PressEOpenGuide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressF1OpenGuide : MonoBehaviour
+{
+    public GameObject guide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        guide.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            guide.SetActive(!guide.activeSelf);
+        }
+    }
+}
