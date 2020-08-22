e7340cfb95da47c1e7668d3201caa6de773ca9c6
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-08-23 02:48:10 +0800

    True End switch scene, need to update

---
+++ Assets/Scripts/CharacterManager/PressFToTalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PressFToTalk : MonoBehaviour
+{
+     private float distance;
+    public Transform origin;
+    public Transform dest;
+    public float interactDistance = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(origin.position, dest.position);
+        if (distance < interactDistance) {
+            if (Input.GetKeyDown(KeyCode.F)) {
+                SceneManager.LoadScene("TrueEndDisplay");
+            }
+        }
+    }
+}
