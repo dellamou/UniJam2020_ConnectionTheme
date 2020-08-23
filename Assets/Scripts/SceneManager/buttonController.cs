de5051454f184afe8499112c319af4c97393e008
Danlei Mou <dmou@student.unimelb.edu.au>
2020-08-23 16:03:52 +0800

    fixed button returning to menu page

--- Assets/Scripts/SceneManager/buttonController.cs
+++ Assets/Scripts/SceneManager/buttonController.cs
@@ -56,4 +56,8 @@ public class buttonController : MonoBehaviour
     {
         SceneManager.LoadScene("TrueEnd");
     }
+    public void menu()
+    {
+        SceneManager.LoadScene("HomePage");
+    }
 }
